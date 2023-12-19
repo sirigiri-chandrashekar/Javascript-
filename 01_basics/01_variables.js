ea98e3aa311d28b3889070684454798472320738
sirigiri-chandrashekar <152269109+sirigiri-chandrashekar@users.noreply.github.com>
2023-12-19 15:21:18 +0000

    pratice of var,letand const .

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 5465165
+let accountEmail ="chandu@gmail.com"
+var accountpassword ="12345"
+accountCity ="Hyderabad"  //don't declare any value without let , var,const . This leads to huge problem .
+let accountState; //If we declare variable and we dont give any value to it ,then it will print undefined
+// accountId = 88  not allowed
+
+accountEmail= "chanskbe@gmail.com"
+accountpassword="454654"
+accountCity="doodhbowli"
+
+console.log(accountId);
+
+ /*
+ prefer not to use var
+ because of issue in block scope and funtional scope
+ */
+
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState]);
