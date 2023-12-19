ba1f57499eeebca30eea5c31cebd881fb7782acd
sirigiri-chandrashekar <152269109+sirigiri-chandrashekar@users.noreply.github.com>
2023-12-19 15:56:12 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as newer version 
+
+//DATATYPES
+
+// number = 1,2235,515.22 (highest value of number datatypes is 2 to the power of 53)
+// bigint = this datatype when it exceeds the limit of number dataype .! it is basically used on big flatforms like trading application, facebook ,google etc.
+// string = "" it is denoted between double quotes and single quotes , but the prefered one is double quote
+// boolean => true/false 
+//null = it is a stand alone value .The variable which has an empty value 
+//undefined = the value which is noit declare to the variable
+//symbol = this datatype is mainly used in react. It can be used for unique 
+//object = {},[],null
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // null lead to object
